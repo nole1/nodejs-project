968b86818713c27ca27c81b3d3c0c5bdc687431e
Animesh <animeshsarma596@gmail.com>
2021-02-09 20:23:59 +0530

    added route and controller for home

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+const homeRoute = require('./routes/index');
+
 // INTERPOLATION
 
 // Understand CALLBACK FUNCTION
+
+
+// use express router
+
+app.use('/',homeRoute);
 
 app.listen(port, function(err)
 {
